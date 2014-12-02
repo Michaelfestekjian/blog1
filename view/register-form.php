c3d984fa7202d1349c2958411c1a16197b21ad4c
Michael Festekjian <festekjianmichael@gmail.com>
2014-12-02 08:44:42 -0800

    made a login 1to the blog

---
+++ view/register-form.php
@@ -0,0 +1,20 @@
+<form>
+    <div>
+        <label for="email">email: </label>
+        <input type="text" name="email />"
+    </div>
+
+    <label for="username">username: </label>
+    <input type="text" name="username />"
+           <div>
+
+        <label for="password">password: </label>
+        <input type="text" name="password />"
+    </div>
+    <button type="submit">Submit</button>
+    <!--makes the submit button that way we can enter our code -->
+    <div>
+
+    </div>
+
+</form>
